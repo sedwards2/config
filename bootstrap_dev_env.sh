4ee2ad6b76fc74171ffaf688d7b5a9a191d95749
Shaun Edwards <shaun@vinecircuit.com>
2016-01-10 20:09:31 -0800

    updated bootstrap_dev_env.sh

--- bootstrap_dev_env.sh
+++ bootstrap_dev_env.sh
@@ -2,22 +2,12 @@
 
 set -e
 
-# note will need to set up password less sudo for this script?
-# sudo visudo
-
 # install git
 sudo apt-get install git
 mkdir -p ~/git
 
 # install chef-dk
-# todo if [ command -v linuxbrew > /dev/null 2>&1 || { exit 1; } ]; then
-# ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
-export PATH="$HOME/.linuxbrew/bin:$PATH"
-export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"
-export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"
-sudo apt-get install build-essential
-brew doctor
-# fi
+curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -c current -P chefdk
 
 if [ -d "~/git/chef" ]; then
     git clone https://github.com/sedwards2/chef
