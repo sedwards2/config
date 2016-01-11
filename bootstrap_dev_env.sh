8c623563f3d2d9ee96b82064dcd03d130911c898
Shaun Edwards <shaun@vinecircuit.com>
2016-01-10 20:49:33 -0800

    updates

--- bootstrap_dev_env.sh
+++ bootstrap_dev_env.sh
@@ -9,10 +9,12 @@ mkdir -p ~/git
 # install chef-dk
 curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -c current -P chefdk
 
+# install my chef repo
 if [ -d "~/git/chef" ]; then
     git clone https://github.com/sedwards2/chef
 fi
 
+# install chef dev env
 cd ~/git/chef
 chef-client -z -o dev
 
