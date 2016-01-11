83ff72c91907fa6ae49efc577d1f8d1ee2c0c1b5
Shaun Edwards <shaun@vinecircuit.com>
2016-01-10 19:53:49 -0800

    updated bootstrap_dev_env.sh

--- bootstrap_dev_env.sh
+++ bootstrap_dev_env.sh
@@ -10,11 +10,11 @@ sudo apt-get install git
 mkdir -p ~/git
 
 # install chef-dk
-if [ command -v linuxbrew > /dev/null 2>&1 || { exit 1; } ]; then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
-    sudo apt-get install build-essential
-    brew doctor
-fi
+# todo if [ command -v linuxbrew > /dev/null 2>&1 || { exit 1; } ]; then
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
+sudo apt-get install build-essential
+brew doctor
+# fi
 
 if [ -d "~/git/chef" ]; then
     git clone https://github.com/sedwards2/chef
